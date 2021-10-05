77f1b968a16cb8b820c4c6b97d4f274b8c82b46d
P <phatnguyen876@gmail.com>
2021-10-06 00:03:19 +0700

    Init BottomBar

---
+++ MissGuided/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MissGuided.Services;
+using MissGuided.Views;
+
+namespace MissGuided
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MissGuided/Views/ShopPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MissGuided.Views
+{
+    public partial class ShopPage : ContentPage
+    {
+        public ShopPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
