a955e5518d29faf4b78a6255211c0a9f23794a4f
P <19521998@gm.uit.edu.vn>
2021-10-15 10:21:56 +0700

    custom color tabbar

---
+++ MissGuided/Services/_APi.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using MissGuided.Models;
+//namespace MissGuided.Services
+//{
+//    public class _APi
+//    {
+//        public async Task<List<TodoItem>> RefreshDataAsync()
+//        {
+//            Uri uri = new Uri(string.Format(Constants.TodoItemsUrl, string.Empty));
+//            HttpResponseMessage response = await client.GetAsync(uri);
+//            if (response.IsSuccessStatusCode)
+//            {
+//                string content = await response.Content.ReadAsStringAsync();
+//                Items = JsonSerializer.Deserialize<List<TodoItem>>(content, serializerOptions);
+//            }
+//}
+//    }
+//}
