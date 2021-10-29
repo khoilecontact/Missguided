09db12d90352b688267f41fc4138124f16e03228
Khoi-1709 <19521709@gm.uit.edu.vn>
2021-10-29 14:29:08 +0700

    29/10/2021

---
+++ MissGuided/Views/CartPageBlank.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MissGuided.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CartPageBlank : ContentPage
+    {
+        public CartPageBlank()
+        {
+            InitializeComponent();
+        }
+
+
+        private void fillmeup_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShopPage());
+        }
+    }
+}
