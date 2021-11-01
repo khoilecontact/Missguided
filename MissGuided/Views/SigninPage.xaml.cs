021587cabec91b861ce24a6844f9b1cb34fc9cfd
KhoiLe <Khoilecontact@gmail.com>
2021-11-01 09:26:38 +0700

    merge main

--- MissGuided/Views/SigninPage.xaml.cs
+++ MissGuided/Views/SigninPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace MissGuided.Views
 {
@@ -16,5 +17,6 @@ namespace MissGuided.Views
         {
             InitializeComponent();
         }
+ 
     }
 }
